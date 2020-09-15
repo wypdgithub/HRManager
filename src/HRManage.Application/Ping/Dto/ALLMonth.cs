01e646314d3582951b9ed5db488cba10a4751b5f
xuezhang11 <2105937858@qq.com>
2020-09-15 17:13:44 +0800

    fff

---
+++ src/HRManage.Application/Ping/Dto/ALLMonth.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using HRManage.Models.Grade;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HRManage.Ping.Dto
+{
+   public class ALLMonth:Profile
+    {
+        public ALLMonth()
+        {
+            CreateMap<GradeDto, Grade>();
+            CreateMap<CreateGradeDto, Grade>();
+
+            CreateMap<MonthleadDto, Monthlead>();
+            CreateMap<CreatMonthleadDto, Monthlead>();
+
+            CreateMap<MonthstaffDto, Monthstaff>();
+            CreateMap<CreatMonthstaffDto, Monthstaff>();
+        }
+    }
+}
