1717ed6c6e0574f261bec57936c9269cf443fe8c
ataoliu <69141308+ataoliu@users.noreply.github.com>
2020-09-15 17:30:28 +0800

    1

--- src/HRManage.Application/AuthorityManagement/Dto/UserInfoDto.cs
+++ src/HRManage.Application/AuthorityManagement/Dto/UserInfoDto.cs
@@ -6,17 +6,11 @@ namespace HRManage.AuthorityManagement.Dto
 {
     public class UserInfoDto : EntityDto<Guid>
     {
-        [StringLength(64)]
         public string Login_Name { get; set; }
-        [StringLength(64)]
         public string PassWord { get; set; }
-        [StringLength(64)]
         public string UserName { get; set; }
-        [StringLength(64)]
         public string Mobile { get; set; }
-        [StringLength(64)]
         public string Email { get; set; }
-        [StringLength(64)]
         public string State { get; set; }
     }
 }
