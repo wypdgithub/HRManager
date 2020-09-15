1b555aea286f4dec04a3d279d9f0bcb6f76c8f6b
wypdgithub <71215481+wypdgithub@users.noreply.github.com>
2020-09-15 14:15:03 +0800

    ll

--- src/HRManage.EntityFrameworkCore/EntityFrameworkCore/HRManageDbContext.cs
+++ src/HRManage.EntityFrameworkCore/EntityFrameworkCore/HRManageDbContext.cs
@@ -3,6 +3,7 @@ using Abp.Zero.EntityFrameworkCore;
 using HRManage.Authorization.Roles;
 using HRManage.Authorization.Users;
 using HRManage.MultiTenancy;
+using HRManager.Entitys;
 
 namespace HRManage.EntityFrameworkCore
 {
@@ -14,5 +15,7 @@ namespace HRManage.EntityFrameworkCore
             : base(options)
         {
         }
+        public DbSet<Detailed> Detailed { get; set; }
+        public DbSet<Sign_In_Tb> Sign_In_Tb { get; set; }
     }
 }
