6fe04935142a759330d9bfc8fc56be8694357140
ataoliu <69141308+ataoliu@users.noreply.github.com>
2020-09-15 14:35:32 +0800

    权限管理

---
+++ src/HRManage.Core/Models/AuthorityManagement/Department.cs
@@ -0,0 +1,17 @@
+﻿using Abp.Domain.Entities.Auditing;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace HRManage.Models.AuthorityManagement
+{
+    /// <summary>
+    /// 部门表
+    /// </summary>
+    public class Department : AuditedAggregateRoot<Guid>
+    {
+        [StringLength(20)]
+        public string Name { get; set; }
+    }
+}
---
+++ src/HRManage.Core/Models/AuthorityManagement/UserPower.cs
@@ -0,0 +1,17 @@
+﻿using Abp.Domain.Entities.Auditing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HRManage.Models.AuthorityManagement
+{
+    /// <summary>
+    /// 用户权限表
+    /// </summary>
+    public class UserPower: AuditedAggregateRoot<Guid>
+    {
+        public Guid User_Id { get; set; }
+        public Guid Power_Id { get; set; }
+        public int Type { get; set; }
+    }
+}
